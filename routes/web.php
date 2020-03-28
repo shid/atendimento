78111b42a173ed3a362752bae09e7ac92cfd7330
shid <lucio@weblink.com.br>
2020-03-27 21:16:05 -0300

    feature/social-authentication-google

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/auth/redirect/{provider}', 'SocialController@redirect');
+Route::get('/callback/{provider}', 'SocialController@callback');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
